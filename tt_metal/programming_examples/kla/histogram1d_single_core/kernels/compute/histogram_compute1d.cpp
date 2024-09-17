1265cb8ecb9ca0298684f43ee6116a4b60fcdce2
root <neelakandan.ramachandran@kla-tencor.com>
2024-09-17 11:30:33 +0000

    Use regs api to acquire and release

--- tt_metal/programming_examples/kla/histogram1d_single_core/kernels/compute/histogram_compute1d.cpp
+++ tt_metal/programming_examples/kla/histogram1d_single_core/kernels/compute/histogram_compute1d.cpp
@@ -11,16 +11,23 @@
 
 #include <cstdint>
 
+
+#define REDUCE_OP PoolType::SUM
+#define REDUCE_DIM ReduceDim::REDUCE_SCALAR
+
 #include "compute_kernel_api.h"
 #include "compute_kernel_api/common.h"
 #include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
 #include "compute_kernel_api/eltwise_unary/sfpu_split_includes.h"
 #include "compute_kernel_api/matmul.h"
+#include "compute_kernel_api/reduce.h"
 #include "compute_kernel_api/tile_move_copy.h"
 #include "debug/dprint.h"
 #include "debug/dprint_tile.h"
 
-// Compute Kernel : perform greater than logic
+
+
+// Compute Kernel : perform the binning logic
 namespace NAMESPACE {
 
 void MAIN {
@@ -33,21 +40,26 @@ void MAIN {
     for (uint32_t tile_index = 0; tile_index < num_tiles; ++tile_index) {
         cb_reserve_back(tt::CB::c_out0, oneTile);
         // Acquire the dest register file for updating them
-        acquire_dst(tt::DstMode::Full);
+        tile_regs_acquire(); 
         // Pop tile after tile, copy to DST and pack
         cb_wait_front(tt::CB::c_in0, 1);
         // copy the tile from input circular buffer to dst register index 0
         copy_tile(tt::CB::c_in0, 0, 0);
-        // Comparison operation to find the mask of indices which are gt the value 728
-        //unary_gt_tile(0, 0x44360000);
+        // Comparison operation to find the mask of indices which are eq to the value 728
         unary_eq_tile(0, 0x44360000);
+        // reduce_tile_math(0);
+        tile_regs_commit();
+        // reduce_tile_math(0, 32);
         // heaviside_tile(0, 5);
         // Pack tile from registers onto destination circular buffer
+        tile_regs_wait(); // packer waits here
         pack_tile(0, tt::CB::c_out0);
+        tile_regs_release();  // packer releases
+
+        
         // move to next tile in circular buffer
         cb_pop_front(tt::CB::c_in0, 1);
-        // release the destination register files for this core
-        release_dst(tt::DstMode::Full); 
+   
         // push the copied tile onto circular buffer slot
         cb_push_back(tt::CB::c_out0, oneTile);
     }
