b0fd04ebf27d0d67aed64119277edf8dee09c930
root <neelakandan.ramachandran@kla-tencor.com>
2024-09-17 11:34:08 +0000

    Reorder the apis for better readability

--- tt_metal/programming_examples/kla/histogram1d_single_core/kernels/compute/histogram_compute1d.cpp
+++ tt_metal/programming_examples/kla/histogram1d_single_core/kernels/compute/histogram_compute1d.cpp
@@ -38,30 +38,29 @@ void MAIN {
     unary_eq_tile_init();
     // heaviside_tile_init();
     for (uint32_t tile_index = 0; tile_index < num_tiles; ++tile_index) {
-        cb_reserve_back(tt::CB::c_out0, oneTile);
+        cb_wait_front(tt::CB::c_in0, 1);      
+        
         // Acquire the dest register file for updating them
         tile_regs_acquire(); 
-        // Pop tile after tile, copy to DST and pack
-        cb_wait_front(tt::CB::c_in0, 1);
         // copy the tile from input circular buffer to dst register index 0
         copy_tile(tt::CB::c_in0, 0, 0);
         // Comparison operation to find the mask of indices which are eq to the value 728
-        unary_eq_tile(0, 0x44360000);
-        // reduce_tile_math(0);
+        unary_eq_tile(0, 0x44360000);   
         tile_regs_commit();
-        // reduce_tile_math(0, 32);
-        // heaviside_tile(0, 5);
-        // Pack tile from registers onto destination circular buffer
+   
+
+        cb_reserve_back(tt::CB::c_out0, oneTile);
         tile_regs_wait(); // packer waits here
         pack_tile(0, tt::CB::c_out0);
         tile_regs_release();  // packer releases
+        // push the copied tile onto circular buffer slot
+        cb_push_back(tt::CB::c_out0, oneTile);
 
         
         // move to next tile in circular buffer
         cb_pop_front(tt::CB::c_in0, 1);
    
-        // push the copied tile onto circular buffer slot
-        cb_push_back(tt::CB::c_out0, oneTile);
+ 
     }
 }
 }  // namespace NAMESPACE
