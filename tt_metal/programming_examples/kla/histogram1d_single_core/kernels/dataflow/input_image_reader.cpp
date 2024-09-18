23736c188ed795ff2a95ca3a0086526ce8ac3566
root <neelakandan.ramachandran@kla-tencor.com>
2024-09-18 00:59:27 +0000

    Add scalar buffer generation for reduction

--- tt_metal/programming_examples/kla/histogram1d_single_core/kernels/dataflow/input_image_reader.cpp
+++ tt_metal/programming_examples/kla/histogram1d_single_core/kernels/dataflow/input_image_reader.cpp
@@ -13,6 +13,23 @@
 
 #include "dataflow_api.h"
 
+
+void generate_bcast_scaler() {
+    constexpr uint32_t cb_intermed_1 = 25U; // CB id 25 reserved for intermediates
+    uint32_t scaler = 1;
+    union { float f; uint32_t u; } u; u.u = scaler;
+    //DPRINT << "basic Scaler = " << F32(u.f) << ENDL();
+    cb_reserve_back(cb_intermed_1, 1);
+    auto ptr = reinterpret_cast<uint16_t*>(get_write_ptr(cb_intermed_1));
+    for (int j = 0; j < 1024; j++)
+        ptr[j] = uint16_t(0);
+
+    for (int k = 0; k < 4; k++)
+    for (int j = 0; j < 16; j++)
+        ptr[k*256 + j] = uint16_t(u.u>>16);
+    cb_push_back(cb_intermed_1, 1);
+}
+
 // kernel_0 : READER for Copying
 void kernel_main() {
     uint32_t src_addr  = get_arg_val<uint32_t>(0); // DRAM address
@@ -25,6 +42,8 @@ void kernel_main() {
     // ublocks size defined in tiles
     constexpr uint32_t ublock_size_tiles = 1;
     uint32_t ublock_size_bytes = get_tile_size(cb_id_in0) * ublock_size_tiles;
+
+    generate_bcast_scaler();
 
     // read a ublock of tiles from src to CB, and then push the ublock to unpacker
     for (uint32_t i = 0; i<num_tiles; i += ublock_size_tiles) {
